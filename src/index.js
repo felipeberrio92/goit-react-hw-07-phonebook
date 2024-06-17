88a13fef5f5b0389f89481082ceb9df8c6222f0d
felipeberrio92 <felipeberriomarin92@gmail.com>
2024-06-17 08:47:43 -0500

    se termina tarea

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from 'components/async_redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <PersistGate loading={null} persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
+
